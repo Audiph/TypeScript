bbb9e825038abfb84d33afb3d494802613701a6f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2022-04-07 18:25:18 +0800

    Added TypeInference/postScore/logMessage functions

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,47 @@
 function startGame() {
   // starting a new game
 
-  const playerName: string = 'Jeff';
+  const playerName: string | undefined = getInputValue('playername');
   logPlayer(playerName);
 
-  let messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+  postScore(80, playerName);
+  postScore(-1, playerName);
 }
 
-function logPlayer(name: any) {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
 }
 
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+  let logger: (value: string) => void;
+
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
+
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;
+
+  logger(`Score: ${score}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+  console.error(err);
+}
